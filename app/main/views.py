6f10841b9527080b8cd1aa10de198fe807100b7d
CarsonFiechtner <fiechtnerc@gmail.com>
2018-09-22 00:59:02 -0600

    Add incentive points

--- app/main/views.py
+++ app/main/views.py
@@ -7,6 +7,7 @@ from . import main
 import binascii
 import hashlib
 import uuid
+import time
 import pymongo
 
 @main.route('/')
@@ -43,7 +44,47 @@ def check_user():
     if(hashed_password != user_exists['password']):
         abort(400)
 
+    if(len(user_exists['email']) > 11 and user_exists['email'][-12:] == "goodwill.org")
+        redirect(url_for('.admin'))
+
     return redirect(url_for('.home'))
+
+@main.route('/add_points', methods=['POST'])
+def add_points():
+    my_db = pymongo.MongoClient(app.config['MONGO_URL']).cfg18_dev_db
+
+    data = request.form
+    print(data)
+
+
+    print(data['pnum'])
+    user_exists = my_db.users.find_one({'pnum': data['pnum']})
+
+    if(not(user_exists)):
+        abort(400)
+
+    if(!data['poll']):
+	id = str(uuid.uuid4())[:4])
+	user_exists['donations'].append({'timestamp': time.time(), 'id': id, 
+	    'pollAnswers': {'what': {'clothes': False, 'furniture': False, 'books': False, 'electronics': False, 'other': False}, 'value': 0}})
+
+    new_points = 0
+    if(user_exists['points'] + 10 >= 100):
+	new_points = 0
+	code = str(uuid.uuid4())[:4])
+	while(my_db.incentives.find_one({'code': code}):
+	    code = str(uuid.uuid4())[:4])
+	my_db.incentives.insert({'timestamp': time.time(), 'code': code, 'pnum': data['pnum'], 'desc': ""})
+	user_exists['incentives'].append(my_db.incentives.find_one({'pnum': data['pnum']}))
+    else:
+        new_points = user_exists['points']+10
+
+    my_db.users.update_one(
+      { "pnum" : data["pnum"] },
+      { $set: { "points" : new_points } }
+    );
+
+    return 
 
 @main.route('/home')
 def home():
