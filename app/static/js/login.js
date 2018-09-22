6c376853457cf87846ec67c423cb0a98be3e5b7a
CarsonFiechtner <fiechtnerc@gmail.com>
2018-09-21 21:24:36 -0600

    Add login.js

---
+++ app/static/js/login.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('#login').on('submit', function(event){
+        console.log($(this));
+        let requestObj = {
+            'email': $('#email').val(),
+            'pass': $('#pass').val()
+        };
+
+        $.ajax({
+            type : 'POST',
+            data : requestObj,
+            url : '/check_user',
+            error: function (xhr, ajaxOptions, thrownError) {
+                if(xhr.status === 400){
+                    $('#acct-dne').show();
+                }
+            }
+        });
+		
+        event.preventDefault();
+    });
+})
