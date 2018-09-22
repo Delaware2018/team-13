4901ac35b9bc6140e5e81f636be6348a8d4996ca
CarsonFiechtner <fiechtnerc@gmail.com>
2018-09-21 19:00:39 -0600

    Fixed route functions

--- app/main/views.py
+++ app/main/views.py
@@ -11,49 +11,49 @@ def index():
     return render_template('index.html')
 
 @main.route('/register')
-def index():
+def register():
     my_db = pymongo.MongoClient(app.config['MONGO_URL']).cfg18_dev_db
 
     return render_template('register.html')
 
 @main.route('/forgotPassword')
-def index():
+def forgotPass():
     my_db = pymongo.MongoClient(app.config['MONGO_URL']).cfg18_dev_db
 
     return render_template('forgotPassword.html')
 
 @main.route('/about')
-def index():
+def about():
     my_db = pymongo.MongoClient(app.config['MONGO_URL']).cfg18_dev_db
 
     return render_template('about.html')
 
 @main.route('/FAQ')
-def index():
+def faq():
     my_db = pymongo.MongoClient(app.config['MONGO_URL']).cfg18_dev_db
 
     return render_template('FAQ.html')
 
 @main.route('/home')
-def index():
+def home():
     my_db = pymongo.MongoClient(app.config['MONGO_URL']).cfg18_dev_db
 
     return render_template('home.html')
 
 @main.route('/resetPassword')
-def index():
+def resetPass():
     my_db = pymongo.MongoClient(app.config['MONGO_URL']).cfg18_dev_db
 
     return render_template('resetPassword.html')
 
 @main.route('/history')
-def index():
+def history():
     my_db = pymongo.MongoClient(app.config['MONGO_URL']).cfg18_dev_db
 
     return render_template('history.html')
 
 @main.route('/settings')
-def index():
+def settings():
     my_db = pymongo.MongoClient(app.config['MONGO_URL']).cfg18_dev_db
 
     return render_template('settings.html')
